99f3f220845ec9eb590739f373d387d076f13a98
glennprays <glennsagala@gmail.com>
2023-11-10 00:32:48 +0700

    add gpc dir

--- flask/app/usecases/audio/youtube_music.py
+++ flask/app/usecases/audio/youtube_music.py
@@ -1,6 +1,11 @@
 from ytmusicapi import YTMusic
+import os
+
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# oauth_json_path = os.path.join(current_dir, "../../../gcp/oauth.json")
 
 ytmusic = YTMusic("./gcp/oauth.json")
+# ytmusic = YTMusic(oauth_json_path)
 
 def search_music (query):
     search_results = ytmusic.search(query=query, filter='songs')
